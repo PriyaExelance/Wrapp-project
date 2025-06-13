c9f58b88d896e09c72d5a0e65d3c2e8a8fd08d12
Priya Dev <priya@example.com>
2025-06-13 14:14:10 +0530

    aaaa

--- src/screens/EmailScreen.tsx
+++ src/screens/EmailScreen.tsx
@@ -26,7 +26,6 @@ import * as Yup from 'yup';
 // my name is priyaaa
 const a = 'arerrererewe';
 const b = 'me and i';
-
 const c = 'me and i';
 type RootStackParamList = {
   MainInfoScreen: undefined;
